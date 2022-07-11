540958a6016eb77dd8c54197cc9a2c6747615dd5
nijiironova <106956718+nijiironova@users.noreply.github.com>
2022-07-11 18:05:59 +0900

    Create catch.js

---
+++ games/js/catch.js
@@ -0,0 +1,54 @@
+function id(elementId) {
+    return document.getElementById(elementId);
+}
+var curso = id('cursor');
+var itemShee = id('itemSheet');
+var clickWai = id('clickWait');
+var screenWidth = 0;
+var itemList = [];
+var itemX = [];
+var itemY = [];
+var itemWidth = 50;
+var cursorWidth = 0;
+var speed = 10;
+var cursorX = 0;
+function localScriptStart(e) {
+    var cr = document.body.getBoundingClientRect();
+    screenWidth = cr.width;
+    cr = curso.getBoundingClientRect();
+    cursorWidth = cr.width;
+    document.addEventListener('mousemove', mouseMoveHandler, false);
+    document.addEventListener('touchmove', touchMoveHandler, false);
+}
+function clickEventHandler(e) {
+    clickWai.style.display = 'none';
+    requestAnimationFrame(moveItem);
+    setTimeout(addItem, Math.floor(Math.random() * 800) + 200);
+}
+function mouseMoveHandler(e) {
+    cursorX = e.clientX;
+    curso.style.left = (cursorX - (cursorWidth / 2))+'px';
+}
+function touchMoveHandler(e) {
+    cursorX = e.clientX;
+    curso.style.left = (cursorX - (cursorWidth / 2))+'px';
+}
+function addItem() {
+    var el = document.createElement('div');
+    var random = Math.floor(Math.random() * (screenWidth - itemWidth));
+    itemList.push(el);
+    itemX.push(random);
+    itemY.push(0);
+    el.style.left = random+'px';
+    itemShee.appendChild(el);
+    setTimeout(addItem, Math.floor(Math.random() * 800) + 200);
+}
+function moveItem() {
+    for(var i = 0; i < itemList.length; i++) {
+        itemY[i] += speed;
+        itemList[i].style.top = itemY[i]+'px';
+        if((cursorX + itemWidth) < itemX[i] && (cursorX - itemWidth) > itemX[i]) {}
+    }
+    requestAnimationFrame(moveItem);
+}
+window.addEventListener('DOMContentLoaded', localScriptStart, false);
