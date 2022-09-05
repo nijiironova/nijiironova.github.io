30586c47c5818f37d93c9eeba8edf2dcbc58af36
nijiironova <106956718+nijiironova@users.noreply.github.com>
2022-09-05 17:43:09 +0900

    Update catch.js

--- games/js/catch.js
+++ games/js/catch.js
@@ -21,7 +21,6 @@ function localScriptStart(e) {
     cursorWidth = cr.width;
     document.addEventListener('mousemove', mouseMoveHandler, false);
     document.addEventListener('touchmove', touchMoveHandler, false);
-    setTimeout(addItem, 3000);
 }
 function clickEventHandler(e) {
     clickWai.style.display = 'none';
